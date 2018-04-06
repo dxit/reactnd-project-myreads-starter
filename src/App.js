a8b519586ae158fb51d1b0f1751f2a9edc20b658
Daniele De Matteo <danieledematteo@gmail.com>
2018-04-06 20:18:19 +0200

    feat: Create search component, rename Shelves component to Library

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Shelves from './Shelves'
+import Library from './Library'
 import Search from './Search'
 
 class BooksApp extends React.Component {
+	state = {
+		books: [],
+		searchResults: []
+	};
+
+	componentDidMount() {
+		BooksAPI.getAll()
+			.then((books) => {
+				this.setState(() => ({
+					books
+				}))
+			})
+	}
+
+	onUpdateShelf = (book, shelf) => {
+		BooksAPI.update(book,shelf)
+			.then(() => this.componentDidMount())
+	};
+
+	onSearch = (query) => {
+		if(query && query.length) {
+			BooksAPI.search(query)
+				.then((results) => {
+					this.setState(() => ({
+						searchResults: results
+					}))
+				});
+		}
+	};
+
 	render() {
 		return (
 			<div className="app">
 				<Route exact path="/" render={() => (
-					<Shelves/>
+					<Library
+						books={this.state.books}
+						onUpdateShelf={(book, shelf) => {
+							this.onUpdateShelf(book, shelf);
+						}}
+					/>
 				)}/>
-				<Route exact path="/search" render={({history}) => (
-					<Search/>
+				<Route path="/search" render={({history}) => (
+					<Search
+						books={this.state.books}
+						searchResults={this.state.searchResults}
+						onSearch={(query) => {
+							console.log(query);
+							this.onSearch(query);
+						}}
+						onUpdateShelf={(book, shelf) => {
+							this.onUpdateShelf(book, shelf);
+						}}
+					/>
 				)}/>
 			</div>
 		)
--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,33 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 class Search extends Component {
+	state = {
+		query: ''
+	};
+
+	updateQuery = (query) => {
+		this.setState(() => ({
+			query: query.trim()
+		}));
+	};
+
+	getShelf = (bookId) => {
+		let book = this.props.books.find((data) => data.id === bookId);
+		return book ? book.shelf : 'none';
+	};
+
+	handleUpdate = (book, shelf) => {
+		if (this.props.onUpdateShelf) this.props.onUpdateShelf(book, shelf);
+	};
+
+	handleSearch = (query) => {
+		if (this.props.onSearch) this.props.onSearch(query);
+	};
+
 	render() {
+		const {query} = this.state;
+		const {searchResults} = this.props;
+
 		return (
 			<div className="search-books">
 				<div className="search-books-bar">
@@ -16,12 +42,44 @@ class Search extends Component {
 		                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
 		                  you don't find a specific author or title. Every search is limited by search terms.
 		                */}
-						<input type="text" placeholder="Search by title or author"/>
+						<input type="text" placeholder="Search by title or author" onChange={(event) => {
+							this.handleSearch(event.target.value);
+							this.updateQuery(event.target.value);
+						}}/>
 
 					</div>
 				</div>
 				<div className="search-books-results">
-					<ol className="books-grid"></ol>
+					{query && query.length && searchResults.length ? (
+						<ol className="books-grid">
+							{searchResults.map((book) => (
+								<li key={book.id}>
+									<div className="book">
+										<div className="book-top">
+											<div className="book-cover" style={{
+												width: 128,
+												height: 193,
+												backgroundSize: 'cover',
+												backgroundPosition: 'center center',
+												backgroundImage: `url("${book.imageLinks ? book.imageLinks.thumbnail : 'https://images-na.ssl-images-amazon.com/images/G/01/author-pages/no-profile-image-placeholder-na._UX250_.png'}")`
+											}}></div>
+											<div className="book-shelf-changer">
+												<select value={book.shelf ? book.shelf : this.getShelf(book.id)} onChange={(event) => this.handleUpdate(book, event.target.value)}>
+													<option value="null" disabled>Move to...</option>
+													<option value="currentlyReading">Currently Reading</option>
+													<option value="wantToRead">Want to Read</option>
+													<option value="read">Read</option>
+													<option value="none">None</option>
+												</select>
+											</div>
+										</div>
+										<div className="book-title">{book.title}</div>
+										<div className="book-authors">{book.authors && book.authors.join(', ')}</div>
+									</div>
+								</li>
+							))}
+						</ol>
+					) : ('')}
 				</div>
 			</div>
 		)
