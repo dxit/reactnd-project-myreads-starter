ee3bbc2864ed45fca6c458716a797e67b0a36648
Daniele De Matteo <danieledematteo@gmail.com>
2018-04-16 21:09:16 +0200

    feat: convert Bookshelf into a stateless functional component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,39 +1,35 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropType from "prop-types";
 import Book from './Book'
 import {Header, Card, Divider} from 'semantic-ui-react'
 
-class Bookshelf extends Component {
-	static propTypes = {
-		title: PropType.string.isRequired,
-		bookshelf: PropType.array.isRequired,
-		books: PropType.array.isRequired,
-		onUpdateShelf: PropType.func.isRequired
-	};
+const Bookshelf = (props) => {
+	return (
+		<div className="bookshelf">
+			<Header as="h2" dividing>{props.title}</Header>
+			{props.bookshelf.length ? (
+				<Card.Group>
+					{props.bookshelf.map((book) => (
+						<Book
+							key={book.id}
+							books={props.books}
+							book={book}
+							onUpdateShelf={props.onUpdateShelf}
+						/>
+					))}
+				</Card.Group>
+			) : (
+				<Divider horizontal>No Books</Divider>
+			)}
+		</div>
+	)
+};
 
-	render() {
-		const {title, bookshelf, books, onUpdateShelf} = this.props;
-
-		return (
-			<div className="bookshelf">
-				<Header as="h2" dividing>{title}</Header>
-				{bookshelf.length ? (
-					<Card.Group>
-						{bookshelf.map((book) => (
-							<Book
-								key={book.id}
-								books={books}
-								book={book}
-								onUpdateShelf={onUpdateShelf}
-							/>
-						))}
-					</Card.Group>
-				) : (
-					<Divider horizontal>No Books</Divider>
-				)}
-			</div>
-		)
-	}
-}
+Bookshelf.propTypes = {
+	title: PropType.string.isRequired,
+	bookshelf: PropType.array.isRequired,
+	books: PropType.array.isRequired,
+	onUpdateShelf: PropType.func.isRequired
+};
 
 export default Bookshelf
